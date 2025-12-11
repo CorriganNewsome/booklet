d97bb1fe98e3a294f4906002b10d1326254ccd6f
CorriganNewsome <corrigannewsome@gmail.com>
2025-12-10 19:58:20 -0500

    added main function to read text from file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def __main__():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+    return file_contents    
+
+__main__()
