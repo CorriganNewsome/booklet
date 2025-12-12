1ac1b375f482954255581bb10d3c55578600d5d9
CorriganNewsome <corrigannewsome@gmail.com>
2025-12-12 06:45:23 -0500

    added stats file and imported function from stats to main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def __main__():
+    from stats import count_words
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    word_count = count_words(book_text)
+    print(word_count)
 
+    
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
-    return file_contents    
+    return file_contents     
 
 __main__()
