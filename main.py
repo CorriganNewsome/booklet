611b748bc184c02fb09882fe7706ef3af45db14f
CorriganNewsome <corrigannewsome@gmail.com>
2025-12-12 19:02:16 -0500

    added the ablity to count letters in a book

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-def __main__():
-    from stats import count_words
+from stats import count_words, num_chars, report
+
+def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = count_words(book_text)
-    print(word_count)
+    number_of_chars = num_chars(book_text)
+    sorted_list = report(number_of_chars)
 
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    
     
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
     return file_contents     
 
-__main__()
+def count_words(text):
+    words = text.split()
+    total_words = len(words)
+    return total_words
+
+main()
--- stats.py
+++ stats.py
@@ -2,3 +2,27 @@ def count_words(text):
     words = text.split()
     total_words = len(words)
     return f"Found {total_words} total words"
+
+''' Start with an empty dictionary,
+Loop over each character in lower_case,
+And update the count for that character in the dictionary? '''
+def num_chars(text):
+    lower_case = text.lower()
+    lower_case_dict = {}
+    for char in lower_case:
+        if char in lower_case_dict:
+            lower_case_dict[char] += 1
+        else:
+            lower_case_dict[char] = 1
+    return lower_case_dict  
+
+
+def sort_on(item):
+    return item["num"]
+
+def report(dict):
+    result = []
+    for i in dict:
+        result.append({"char": i, "num": dict[i]})
+    result.sort(key=sort_on, reverse = True)
+    return result    
